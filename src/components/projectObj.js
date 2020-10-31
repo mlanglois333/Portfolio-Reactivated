ab9426fd30b828e240dfe64fac67127a0c8ffe39
Marc <mlanglois333@icloud.com>
2020-10-31 12:14:27 -0400

    init

---
+++ src/components/projectObj.js
@@ -0,0 +1,45 @@
+const projects = [
+
+    {
+        title: "",
+        description: "",
+        github: "",
+        deployed: "",
+        imgUrl: "",
+    },
+    {
+        title: "",
+        description: "",
+        github: "",
+        deployed: "",
+        imgUrl: "",
+    }, {
+        title: "",
+        description: "",
+        github: "",
+        deployed: "",
+        imgUrl: "",
+    }, {
+        title: "",
+        description: "",
+        github: "",
+        deployed: "",
+        imgUrl: "",
+    }, {
+        title: "",
+        description: "",
+        github: "",
+        deployed: "",
+        imgUrl: "",
+    }, {
+        title: "",
+        description: "",
+        github: "",
+        deployed: "",
+        imgUrl: "",
+    }
+
+
+];
+
+exports.projects= projects;
