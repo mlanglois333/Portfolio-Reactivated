15cdbc88815abcea81fee75ede4276aa23d988bc
Marc <mlanglois333@icloud.com>
2020-11-10 13:20:48 -0500

    added props

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(props){
     return (<div>
         
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#about">Marc Langlois</a>
+        <a className="navbar-brand" href="/" onClick={props.home}>Marc Langlois</a>
         <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -14,10 +14,10 @@ function Header(props){
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                    <a className="nav-link" href="#applications">Applications</a>
+                    <a className="nav-link" href="/" onClick={props.portfolio}>Applications</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#about">About</a>
+                    <a className="nav-link" href="/" onClick={props.about}>About</a>
                 </li>
             </ul>
         </div>
