31c289052fb6e8be39803c93f65a20ef89986265
Marc <mlanglois333@icloud.com>
2020-12-16 12:13:42 -0500

    Reformatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(props) {
 
     return (<div>
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-inverse">
             <a className="navbar-brand" href="/" onClick={props.home}>Marc Langlois</a>
             
             <div id="navbarSupportedContent">
@@ -17,7 +17,7 @@ function Header(props) {
                     </li>
                 </ul>
             </div>
-            <div className="float-right">
+            <div className="navbar-right">
             <a href="https://www.linkedin.com/in/marc-langlois-2b40b61b0/"><img src="./img/liImg.png" alt="linkedIn" className="linkImg" /></a>
         <a href='https://github.com/mlanglois333'><img src= "./img/github.png" alt="gitHub" className="linkImg" /></a>
             </div>
