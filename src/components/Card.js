27fc3d3f06d31eb799d60c95694f462cc4c28533
Marc <mlanglois333@icloud.com>
2020-12-16 17:50:35 -0500

    design

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,44 @@
 import React from "react";
 import Fade from 'react-reveal/Fade';
+import Container from './Container';
+import Row from './Row';
+import Col from './Col';
 
 function Card(props) {
   return (<div>
-    <Fade cascade>
-  
-        <h2>{props.header}</h2>
-        <br />
-        <img src={props.image} alt="screenshot" className="img-fluid screenshot" />
-        <p>
-          {props.detail}
-          </p>
-          <br />
-        <p><img src={props.left} alt="left" onClick={props.leftClick} className="buttonImg" /> || {props.li} || {props.gh} || <img src={props.right} alt="left" onClick={props.rightClick} className="buttonImg" /></p>
-      </Fade>
-    </div>
-    
+    <Container>
+      <Row>
+        <Col size="md-12">
+          <p><img src={props.left} alt="left" onClick={props.leftClick} className="buttonImg" /> || {props.li} || {props.gh} || <img src={props.right} alt="left" onClick={props.rightClick} className="buttonImg" /></p>
+
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-6">
+          <Fade clear>
+            <img src={props.image} alt="screenshot" className="img-fluid screenshot" />
+          </Fade>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+
+
+          <Fade cascade>
+
+            <h2>{props.header}</h2>
+            <br />
+
+            <p>
+              {props.detail}
+            </p>
+          </Fade>
+        </Col>
+      </Row>
+
+
+
+    </Container></div>
   );
 }
 
