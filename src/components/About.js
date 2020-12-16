8132c2333df0e2fceedca2f6abe01d2dc1dc2e68
Marc <mlanglois333@icloud.com>
2020-12-16 18:22:40 -0500

    formatting

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About() {
     return (
     <div  className="hero-text">
                    
-            <iframe title="resume" src="https://docs.google.com/document/d/e/2PACX-1vQ0pnRqBQaCZtFW_bBqRDTAB1aQ8BYDMQfP5pxS-UiRX2q036Yspefq547XYa6gbzPur8Y9B4B-LxGM/pub?embedded=true"></iframe>
+            <iframe title="resume" src="https://docs.google.com/document/d/e/2PACX-1vQ0pnRqBQaCZtFW_bBqRDTAB1aQ8BYDMQfP5pxS-UiRX2q036Yspefq547XYa6gbzPur8Y9B4B-LxGM/pub?embedded=true" width="960" height="1240" allowfullscreen="true"
+mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
     </div>)
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,22 +6,14 @@ import Col from './Col';
 function Card(props) {
   return (<div>
     <Container>
-      <Row>
-        <Col size="md-12">
-          <p><img src={props.left} alt="left" onClick={props.leftClick} className="buttonImg" /> || {props.li} || {props.gh} || <img src={props.right} alt="left" onClick={props.rightClick} className="buttonImg" /></p>
-
-        </Col>
-      </Row>
+      
       <Row>
         <Col size="md-6">
           
             <img src={props.image} alt="screenshot" className="img-fluid screenshot" />
          
         </Col>
-        <Col size="md-6">
-
-
-         
+        <Col size="md-6">   
 
             <h2>{props.header}</h2>
             
@@ -30,6 +22,12 @@ function Card(props) {
               {props.detail}
             </p>
           
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-12">
+          <p><img src={props.left} alt="left" onClick={props.leftClick} className="buttonImg" /> || {props.li} || {props.gh} || <img src={props.right} alt="left" onClick={props.rightClick} className="buttonImg" /></p>
+
         </Col>
       </Row>
 
