553f92733fea466067bbbc225b0cc0895714a4a3
Marc <mlanglois333@icloud.com>
2020-12-16 19:04:33 -0500

    link style

--- src/components/About.js
+++ src/components/About.js
@@ -12,16 +12,20 @@ function About() {
             <Row>
                 <Col size="md-6">
 
-                    <img src="./img/profilepic.JPG" alt="author"></img>
+                    <img className="profilePic" src="./img/profilepic.JPG" alt="author"></img>
                 </Col>
                 <Col size="md-6">
                     <p>
-                        Thank you for stopping by!
+                        <span className="emph">Thank you for stopping by!</span>
                         <br />
+                        <br />
+
 
                         I'm a junior software engineer experienced in agile methodologies and proficient with JavaScript,  
                         Node, React, MySql, ORMs, TypeScript, CSS frameworks, and other related technologies.
                         <br />
+                        <br />
+
 
                         I'm a strong communicator, creative problem-solver, and critical thinker.  
                         I have effective leadership skills and I'm always a dedicated team player in group settings.
@@ -30,6 +34,8 @@ function About() {
                         I have ten years management experience in the restaurant business with a 
                         demonstrated ability to work efficiently and effectively while multitasking.
                         <br />
+                        <br />
+
 
                         Please feel free to reach out any time!
 
