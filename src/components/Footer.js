77336959adde98a72ff8a6d6ff9c4ff439402675
Marc <mlanglois333@icloud.com>
2020-12-21 14:04:32 -0500

    pull img right

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,18 @@ function Footer() {
 
     <div className="footy">
 
-      <a href="https://www.linkedin.com/in/marc-langlois-2b40b61b0/"><img src="./img/liImg.png" alt="linkedIn" className="linkImg" /></a>
-
-      <a href='https://github.com/mlanglois333'><img src="./img/github.png" alt="gitHub" className="linkImg" /></a>
+     
 
 
 
        Marc Langlois || MLangloisCoding@gmail.com || 2020 
-    <a href="https://mlanglois333.github.io/Portfolio-Reactivated/"> Homepage</a>
+    
+    <div className="pull-right">
        
+ <a href="https://www.linkedin.com/in/marc-langlois-2b40b61b0/"><img src="./img/liImg.png" alt="linkedIn" className="linkImg" /></a>
 
-
+      <a href='https://github.com/mlanglois333'><img src="./img/github.png" alt="gitHub" className="linkImg" /></a>
+</div>
     </div>
 
 
