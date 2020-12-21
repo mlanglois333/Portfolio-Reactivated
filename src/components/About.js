28bddc762305e179c71573518878d3750be56c34
Marc <mlanglois333@icloud.com>
2020-12-21 15:26:39 -0500

    button pos

--- src/components/About.js
+++ src/components/About.js
@@ -6,33 +6,31 @@ import Col from "./Col";
 
 function About() {
     return (
-        
+
         <Container>
 
-            
-                <Row>
-            <Col size="sm-6 xs-6" children={
-                    <img className="profilePic" src="./img/image1.jpeg" alt="author"></img>
-                } /> 
 
-                <Col size="sm-6 xs-6" children={
-                  <p>
+            <Row>
+
+
+                <Col size="sm-6 xs-12" children={
+                    <p>
                         <span className="emph">Thank you for stopping by!</span>
                         <br />
                         <br />
 
 
-                        I'm a junior software engineer experienced in agile methodologies and proficient with JavaScript,  
+                        I'm a junior software engineer experienced in agile methodologies and proficient with JavaScript,
                         Node, React, MySql, ORMs, TypeScript, CSS frameworks, and other related technologies.
                         <br />
                         <br />
 
 
-                        I'm a strong communicator, creative problem-solver, and critical thinker.  
+                        I'm a strong communicator, creative problem-solver, and critical thinker.
                         I have effective leadership skills and I'm always a dedicated team player in group settings.
                         <br />
-                        
-                        I have ten years management experience in the restaurant business with a 
+
+                        I have ten years management experience in the restaurant business with a
                         demonstrated ability to work efficiently and effectively while multitasking.
                         <br />
                         <br />
@@ -40,13 +38,15 @@ function About() {
 
                         Please feel free to reach out any time!
 
-                    </p>  
+                    </p>
                 } />
+                <Col size="sm-6 xs-6" children={
+                    <img className="profilePic" src="./img/image1.jpeg" alt="author"></img>
+                } />
+            </ Row>
+        </Container>
 
-                </ Row>
-                </Container>
-                
-                )
+    )
 }
 
 export default About;
